f7f8eb00d8cbd8909e8d0857a5e24ea21766d874
nielsdelestinne <nielsdelestinne@gmail.com>
2018-11-04 13:07:58 +0100

    auto generate indeX.html page

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
@@ -18,5 +19,10 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'My Custom Title'
+        })
+    ]
 };
